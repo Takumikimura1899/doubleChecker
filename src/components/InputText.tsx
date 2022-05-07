bf0b43e4cd2274d3ef6b222f5ae78728be9f3207
taku1219 <acmiran189926@gmail.com>
2022-05-07 11:54:00 +0900

    [add] count text.length

--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -2,5 +2,22 @@ import React, { useState } from 'react';
 
 export const InputText = () => {
   const [text, setText] = useState<string>('Input Text Default');
-  return <div>{text}</div>;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+    console.log(text);
+  };
+  return (
+    <>
+      <form action=''>
+        <label htmlFor=''>正しいテキストを入力</label>
+        <input
+          type='text'
+          placeholder='ここに正しいテキストを入力してください'
+          onChange={handleChange}
+        />
+        <span>{`文字数:${text.length}`}</span>
+      </form>
+    </>
+  );
 };
