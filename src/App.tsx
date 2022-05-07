4600eea52ab3713b85373c638a17a2a664edac96
taku1219 <acmiran189926@gmail.com>
2022-05-07 11:44:19 +0900

    initialCommit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <p>仮</p>
+    </>
+  );
+}
+
+export default App;
