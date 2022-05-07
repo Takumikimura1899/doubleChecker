45675e823916939513e21473a5165304a6b2587a
taku1219 <acmiran189926@gmail.com>
2022-05-07 16:09:51 +0900

    [add] ２つ表示するように

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,16 @@ import './App.css';
 import { InputText } from './components/InputText';
 
 function App() {
+  const [text, setText] = useState<string>('Input Text Default');
+  const [reInputText, setReInputText] = useState('second Input Text Default');
+
+  const handleTextTrim = (text: string) => {
+    setText(text.trim());
+  };
   return (
     <>
-      <InputText />
+      <InputText text={text} setText={setText} />
+      <InputText text={reInputText} setText={setReInputText} />
     </>
   );
 }
--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -1,31 +1,35 @@
 import React, { useState } from 'react';
 
-export const InputText = () => {
-  const [text, setText] = useState<string>('Input Text Default');
+type Props = {
+  text: string;
+  setText: React.Dispatch<React.SetStateAction<string>>;
+};
 
+export const InputText: React.FC<Props> = ({ text, setText }) => {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setText(event.target.value);
     console.log(text);
   };
-
   const handleTextTrim = (text: string) => {
     setText(text.trim());
   };
   return (
     <>
-      <form action=''>
-        <label htmlFor=''>正しいテキストを入力</label>
-        {text.includes(' ') && <p>半角スペースが含まれています</p>}
-        {text.includes('　') && <p>全角スペースが含まれています</p>}
-        <input
-          type='text'
-          placeholder='ここに正しいテキストを入力してください'
-          onChange={handleChange}
-          value={text}
-        />
-        <span>{`文字数:${text.length}`}</span>
-      </form>
-      <button onClick={() => handleTextTrim(text)}>スペースを削除する</button>
+      <div className='inputArea'>
+        <form action='' className='inputArea-form'>
+          <label htmlFor=''>正しいテキストを入力</label>
+          {text.includes(' ') && <p>半角スペースが含まれています</p>}
+          {text.includes('　') && <p>全角スペースが含まれています</p>}
+          <input
+            type='text'
+            placeholder='ここに正しいテキストを入力してください'
+            onChange={handleChange}
+            value={text}
+          />
+          <span>{`文字数:${text.length}`}</span>
+        </form>
+        <button onClick={() => handleTextTrim(text)}>スペースを削除する</button>
+      </div>
     </>
   );
 };
